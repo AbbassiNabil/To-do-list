1d690661fc886d1bd3b64b5e84ee6c268c0162c1
Belén <etudebelen84@gmail.com>
2026-05-20 13:17:33 +0200

    Implement CRUD operations for to-do items and remove obsolete repositoryFunction.php

--- index.php
+++ index.php
@@ -4,22 +4,18 @@ require_once 'repositoryFunction.php';
 $method = $_SERVER['REQUEST_METHOD'];
 $uri = $_SERVER['REQUEST_URI'];
 
-$database = connexion();//Fonction de connexion
-switch ($method) { 
-    case 'GET': 
-        if ($uri === "/todo"){ //Preparation/exécution/récupération des résultat
+global $database;
+
+switch(true){
+    case $method == 'GET' && $uri == "/todo"{
             $stmt = $database->prepare('SELECT * FROM todo');
             $stmt->execute();
             $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        } else {
-            http_response_code(404);
-                echo json_encode(["message" => "Route non trouvée"]);
-        }
-        break; 
+            break;
+    }
 
-    case 'POST': 
-        if($uri === "/todolist"){
-            $body = json_decode(file_get_contents('php://input'), true); 
+    case $method == 'POST' && $uri == "/todolist"{
+        $body = json_decode(file_get_contents('php://input'), true); 
             $stmt = $database->prepare('INSERT INTO todo (title, description, done) VALUES (:title, :description, :done)'); //Paramétres nommés
             $stmt->execute([
                 ':title' => $body['title'],
@@ -28,23 +24,63 @@ switch ($method) {
             ]);
             http_response_code(201);
                 echo json_encode(["message" => "Created - ressource créée avec succès"]);
-        } else {
-           http_response_code(404);
-                echo json_encode(["message" => "Route non trouvée"]); 
-        }
-        break;
+            break;
+    }
 
-    case 'PUT': //Modification de la page existante
-        if($uri === "PUT /todolist/{uuid}/todo"){
+    default : 
+            http_response_code(404);
+            echo json_encode(["message" => "Route non trouvée"]);
 
-        } else{
-
-        }
-        break; 
-    case 'DELETE': 
-        break; 
-    case 'PATCH': 
-        break;
-    default:
-        break;
 }
+// switch ($method) { 
+//     case 'GET': 
+//         if ($uri === "/todo"){ //Preparation/exécution/récupération des résultat
+//             $stmt = $database->prepare('SELECT * FROM todo');
+//             $stmt->execute();
+//             $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+//         } else {
+//             http_response_code(404);
+//                 echo json_encode(["message" => "Route non trouvée"]);
+//         }
+//         break; 
+
+//     case 'POST': 
+//         if($uri === "/todolist"){
+//             $body = json_decode(file_get_contents('php://input'), true); 
+//             $stmt = $database->prepare('INSERT INTO todo (title, description, done) VALUES (:title, :description, :done)'); //Paramétres nommés
+//             $stmt->execute([
+//                 ':title' => $body['title'],
+//                 ':description' => $body['description'],
+//                 ':done' => $body['done']
+//             ]);
+//             http_response_code(201);
+//                 echo json_encode(["message" => "Created - ressource créée avec succès"]);
+//         } else {
+//            http_response_code(404);
+//                 echo json_encode(["message" => "Route non trouvée"]); 
+//         }
+//         break;
+
+//     case 'PUT': //Modification de la page existante / Même strucutre que pour le POST
+//         if($uri == "/todolist/{uuid}/todo"){
+//             $body = json_decode(file_get_contents('php://input'), true); 
+//             $stmt = $database->prepare("UPDATE todo SET title = :title, description = :description, done = :done WHERE id = :id");
+//             $stmt->execute([
+//                 ':title' => $body['title'],
+//                 ':description' => $body['description'],
+//                 ':done' => $body['done']
+//             ]);
+//             http_response_code(200);
+//                 echo json_encode(["message" => "Update - ressource créée avec succès"]);
+//         } else{
+//             http_response_code(404);
+//                 echo json_encode(["message" => "Route non trouvée"]);
+//         }
+//         break; 
+//     case 'DELETE': 
+//         break; 
+//     case 'PATCH': 
+//         break;
+//     default:
+//         break;
+// }
