697e2e60b0f1ded37ed3402bf0df0f2000a034b0
Belén <etudebelen84@gmail.com>
2026-05-20 11:22:18 +0200

    Add initial implementation of API for managing to-do items

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+require_once 'model.php';
+
+$method = $_SERVER['REQUEST_METHOD'];
+$uri = $_SERVER['REQUEST_URI'];
+
+$database = connexion();//Fonction de connexion
+switch ($method) { 
+    case 'GET': 
+        if ($uri === "/todo"){ //Preparation/exécution/récupération des résultat
+            $stmt = $database->prepare('SELECT * FROM todo');
+            $stmt->execute();
+            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        } else {
+            http_response_code(404);
+                echo json_encode(["message" => "Route non trouvée"]);
+        }
+        break; 
+
+    case 'POST': 
+        if($uri === "/todolist"){
+            $body = json_decode(file_get_contents('php://input'), true); 
+            $stmt = $database->prepare('INSERT INTO todo (title, description, done) VALUES (:title, :description, :done)'); //Paramétres nommés
+            $stmt->execute([
+                ':title' => $body('title'),
+                ':description' => $body['description'],
+                ':done' => $body['done']
+            ])
+            $stmt->fetchAll(PDO::FETCH_ASSOC);
+            http_response_code(201);
+                echo json_encode(["message" => "Created - ressource créée avec succès"]);
+        } else {
+            
+        }
+        break;
+    case 'PUT': 
+        break; 
+    case 'DELETE': 
+        break; 
+    case 'PATCH': 
+        break;
+    default:
+        break;
+}
---
+++ model.php
@@ -0,0 +1,8 @@
+<?php
+require_once 'repositoryFunction.php';
+
+define('DB_HOST', '');
+define('DB_PORT', '3306');
+define('DB_DATABASE', 'blog_greta');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
